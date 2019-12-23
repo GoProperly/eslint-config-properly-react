2b17a98468d29dbda219c43ea32f53abd35127ee
Gavin Sharp <gavin@goproperly.com>
2019-12-23 16:16:31 -0500

    release 1.0

--- release.config.js
+++ release.config.js
@@ -2,15 +2,12 @@ module.exports = {
   plugins: [
     ['@semantic-release/commit-analyzer', {
       releaseRules: [
-        // All commits produce patch releases
-        { release: 'patch' },
+        // All commits produce major releases (for v1)
+        { release: 'major' },
       ],
     }],
     '@semantic-release/changelog',
-    ['@semantic-release/npm', {
-      // We publish manually via a GitHub workflow
-      npmPublish: false,
-    }],
+    '@semantic-release/npm',
     '@semantic-release/git',
     '@semantic-release/github',
   ],
