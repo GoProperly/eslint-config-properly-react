50af28bcc79a26f4862f37d69ded852f5bad09c0
Gavin Sharp <gavin@goproperly.com>
2020-01-12 00:36:51 -0500

    build: update release config to generate release notes automatically, use conventionalcommits

--- release.config.js
+++ release.config.js
@@ -1,19 +1,11 @@
 module.exports = {
+  preset: 'conventionalcommits',
   plugins: [
-    [
-      '@semantic-release/commit-analyzer',
-      {
-        releaseRules: [
-          // By default, generate a patch release
-          { release: 'patch' },
-          // allow specifying e.g. 'chore(no-release)' to skip releasing
-          { scope: 'no-release', release: false },
-        ],
-      },
-    ],
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/npm',
     '@semantic-release/git',
     '@semantic-release/github',
+    '@semantic-release/npm',
   ],
 };
