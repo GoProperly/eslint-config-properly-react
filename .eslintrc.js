3e743cd5a6dc23776c792f10d3b31c6ad8f0d62b
Gavin Sharp <gavin@properly.ca>
2020-10-02 10:45:59 -0400

    chore: specify Jest version explicitly in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,14 @@
-module.exports = require('./index');
+// We lint ourselves with our own rules!
+const baseConfig = require('./index');
+
+module.exports = {
+  ...baseConfig,
+  // Because this repository does not have Jest installed (but consumers typically do), we need to
+  // explicitly tell eslint-plugin-jest what Jest version to use for its no-deprecated-functions
+  // rule.
+  settings: {
+    jest: {
+      version: 26,
+    },
+  },
+};
