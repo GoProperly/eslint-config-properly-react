ef3cf1fe06c8f9712d17ae4beb0db53149ee8aeb
Robbie Averill <robbie.averill@properly.ca>
2023-01-18 11:20:48 -0800

    feat: add no-unstable-nested-components rule as an error
    This prevents devs from defining components inside components, which results in re-rendering and resetti
    ng the state of the child components when the parent component re-renders

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ module.exports = {
   rules: {
     // Don't want to rename all our files
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+    'react/no-unstable-nested-components': 'error',
     'jest/expect-expect': [
       'warn',
       {
